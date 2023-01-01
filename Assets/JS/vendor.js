3d2607125b9f6a4c6235a6eb8abfd42d67182042
Patel Mohd Ali <patel9619ali@gmail.com>
2023-01-01 21:11:41 +0530

    New Map Project

---
+++ Assets/JS/vendor.js
@@ -0,0 +1,12 @@
+let navigation = navigator.geolocation.getCurrentPosition(function(position){
+   alert(position);
+},function(position){
+   alert(position);
+})
+
+let map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+console.log(navigation)
