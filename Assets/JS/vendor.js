ee25af0bcbcbdff562fe04ef6fcc4fb37c066369
Patel Mohd Ali <patel9619ali@gmail.com>
2023-01-02 01:19:38 +0530

    02-01-2022

--- Assets/JS/vendor.js
+++ Assets/JS/vendor.js
@@ -1,12 +1,16 @@
-let navigation = navigator.geolocation.getCurrentPosition(function(position){
-   alert(position);
-},function(position){
-   alert(position);
-})
+navigator.geolocation.getCurrentPosition(function(position){
+   const coordinates = position.coords;
+   let {latitude,longitude} = coordinates;
+   console.log(`${latitude} and ${longitude}`)
+},function(){
+
+});
+
 
 let map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-console.log(navigation);
+console.log(map);
+console.log(L);
