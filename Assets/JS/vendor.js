e5594fef99ecbbe840736ff26e1546133bc00928
Patel Mohd Ali <patel9619ali@gmail.com>
2023-01-02 00:44:28 +0530

    Latest

--- Assets/JS/vendor.js
+++ Assets/JS/vendor.js
@@ -9,4 +9,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-console.log(navigation)
+console.log(navigation);
